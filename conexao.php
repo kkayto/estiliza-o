1b9cca1052c937b69051bf3b0b9275479e9731ea
kkayto <heloisaauletta280@gmail.com>
2026-06-24 18:15:20 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,32 +1,40 @@
 <?php
+
 class BancodeDados {
-    // Nas linhas abaixo você poderá colocar as informações do Banco de Dados.
+
     private $host = "localhost:3308";
-   // private $host = "localhost"; 	// Nome ou IP do Servidor
-    private $user = "root"; 		// Usuário do Servidor MySQL
-    private $senha = ""; 		// Senha do Usuário MySQL
-    private $banco = "testeaula"; 		// Nome do seu Banco de Dados
+    private $user = "root";
+    private $senha = "";
+    private $banco = "testeaula";
+
     public $con;
 
-	// método responsável para conexão a base de dados
-	function conecta(){
-        $this->con = mysqli_connect($this->host,$this->user,$this->senha, $this->banco);
-       //   $this->con = @mysqli_connect($this->host,$this->user,$this->senha, $this->banco);
-        // Conecta ao Banco de Dados
-        if(!$this->con){
-      		// Caso ocorra um erro, exibe uma mensagem com o erro
-			die ("Problemas com a conex&atildeo");
+    // Conectar ao banco
+    public function conecta() {
+
+        $this->con = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->senha,
+            $this->banco
+        );
+
+        if (!$this->con) {
+            die("Erro na conexÃ£o: " . mysqli_connect_error());
         }
+
+        mysqli_set_charset($this->con, "utf8");
     }
 
-	// método responsável para fechar a conexão
-	function fechar(){
-		mysqli_close($this->con);
-		return;
-	}
+    // Executa qualquer SQL
+    public function query($sql) {
+        return mysqli_query($this->con, $sql);
+    }
 
-
+    // Fecha a conexÃ£o
+    public function fechar() {
+        mysqli_close($this->con);
+    }
 }
-
 
 ?>
